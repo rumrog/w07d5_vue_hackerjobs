3d6f9679d1e7ca2eefdfdce1348c4045542bfc4e
Rumen Roger <rumenroger@gmail.com>
2020-06-28 23:19:17 +0100

    Jobs list displayed

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <div id="app">
+    <h1>Hacker Jobs</h1>
+    <jobs-list :jobs='jobs'></jobs-list>
   </div>
 </template>
 
 <script>
+import {eventBus} from './main.js'
+import ListComponent from './components/ListComponent.vue'
+import JobsList from './components/JobsList.vue'
 
 export default {
   name: 'App',
-  components: {
-    
+  data() {
+    return {
+      jobs: [],
+      selectedJob: null
+    }
+  },
+    mounted() {
+      fetch("https://api.hackerwebapp.com/jobs?page=1")
+      .then(res => res.json())
+      .then(jobs => this.jobs = jobs)
+
+      eventBus.$on('job-selected', (job) => {
+        console.log(job.title);
+
+        this.selectedJob = job
+      })
+    },
+    components: {
+    "jobs-list": JobsList
   }
 }
 </script>
 
 <style>
-html {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  font-family: Verdana, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: #121212;
+  color: white;
 }
 </style>
---
+++ src/components/ListComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <li v-on:click='handleClick'><h2>{{ job.title }}</h2></li>
+</template>
+
+<script>
+import {eventBus} from '../main.js'
+
+export default {
+  name: 'list-component',
+  props: ['job'],
+  methods: {
+    handleClick(){
+      eventBus.$emit('job-selected', this.job)
+    }
+  }
+};
+</script>
